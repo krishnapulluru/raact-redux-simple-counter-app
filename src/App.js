f7a5b297258fef688cc9a6d0b4a8a6911c4d0715
KrishnaPulluru <85900329+krishnapulluru@users.noreply.github.com>
2022-08-05 23:00:24 +0530

    basic counter app using react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
+export default function App() {
+    const counter = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let increment = () => {
+        dispatch(actions.increment());
+    };
+    let decrement = () => {
+        dispatch(actions.decrement());
+    };
+    let addBy = () => {
+        dispatch(actions.addBy(10));
+    };
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <h2>{counter}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={addBy}>Add By 10</button>
+        </div>
+    );
 }
-
-export default App;
